71097b12bb4d305031dece1b6b7d1e3cff8ac6c3
Takashi Takebayashi <changesworlds@gmail.com>
2020-08-18 13:08:32 +0900

    Tweak

--- scraping.rb
+++ scraping.rb
@@ -37,52 +37,52 @@ File.open(file, mode = 'w') do |f|
       element.css('.shomon1').each_with_index do |element, idx|
         shomon1 = element.content
       end
+      # doc sanitize
+      doc = Sanitize.clean(element.inner_html, :elements => %w{
+          math
+          maction
+          maligngroup
+          malignmark
+          menclose
+          merror
+          mfenced
+          mfrac
+          mglyph
+          mi
+          mlabeledtr
+          mlongdiv
+          mmultiscripts
+          mn
+          mo
+          mover
+          mpadded
+          mphantom
+          mroot
+          mrow
+          ms
+          mscarries
+          mscarry
+          msgroup
+          msline
+          mspace
+          msqrt
+          msrow
+          mstack
+          mstyle
+          msub
+          msup
+          msubsup
+          mtable
+          mtd
+          mtext
+          mtr
+          munder
+          munderover
+          semantics
+          annotation
+          annotation-xml}
+      )
     end
-    # doc sanitize
-    doc = Sanitize.clean(element.inner_html, :elements => %w{
-        math
-        maction
-        maligngroup
-        malignmark
-        menclose
-        merror
-        mfenced
-        mfrac
-        mglyph
-        mi
-        mlabeledtr
-        mlongdiv
-        mmultiscripts
-        mn
-        mo
-        mover
-        mpadded
-        mphantom
-        mroot
-        mrow
-        ms
-        mscarries
-        mscarry
-        msgroup
-        msline
-        mspace
-        msqrt
-        msrow
-        mstack
-        mstyle
-        msub
-        msup
-        msubsup
-        mtable
-        mtd
-        mtext
-        mtr
-        munder
-        munderover
-        semantics
-        annotation
-        annotation-xml}
-    )
     f.write("#{refinfo},#{subject_name},#{qnum},#{shomon1},#{score},#{doc.gsub(/[\r\n]/,"").strip}\n")
   end
 end
