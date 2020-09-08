74b7340acd93a1f1aab65a81344936f1eb3489df
Takashi Takebayashi <changesworlds@gmail.com>
2020-09-08 09:24:32 +0900

    Modify url -> urls

--- scraping.rb
+++ scraping.rb
@@ -4,85 +4,87 @@ require 'nokogiri'
 require 'open-uri'
 require 'sanitize'
 
-url = ARGV[0]
-file = ARGV[1]
+urls = ['https://www.google.com/']
+file = ARGV[0]
 
-doc = Nokogiri::HTML(URI.open(url))
-File.open(file, mode = 'a') do |f|
-  doc.css("div.totaldiv").each do |element|
-    refinfo, subject, score, qnum, shomon1, doc = ''
-    refinfo
-    # refinfo
-    element.css('div.refinfo').each do |element|
-      refinfo = element.content.strip
-    end
-    # refinfo2
-    element.css('div.refinfo2').each do |element|
-      # subject
-      idx_1st = element.to_html.strip.index('数学')
-      idx_2nd = element.to_html.strip.index('</')
-      subject = element.to_html.strip[idx_1st, idx_2nd - idx_1st]
-      # score
-      idx_1st = element.content.strip.index('配点')
-      idx_2nd = element.content.strip.index('点', idx_1st+2)
-      score = element.content.strip[idx_1st+2, idx_2nd - (idx_1st+2)]
-    end
-    # qbody
-    element.css('div.qbody').each do |element|
-      # qnum
-      element.css('.qnum').each do |element|
-        qnum = element.content
+urls.each do |url|
+  doc = Nokogiri::HTML(URI.open(url))
+  File.open(file, mode = 'a') do |f|
+    doc.css("div.totaldiv").each do |element|
+      refinfo, subject, score, qnum, shomon1, doc = ''
+      refinfo
+      # refinfo
+      element.css('div.refinfo').each do |element|
+        refinfo = element.content.strip
       end
-      # shomon1
-      element.css('.shomon1').each_with_index do |element, idx|
-        shomon1 = element.content
+      # refinfo2
+      element.css('div.refinfo2').each do |element|
+        # subject
+        idx_1st = element.to_html.strip.index('数学')
+        idx_2nd = element.to_html.strip.index('</')
+        subject = element.to_html.strip[idx_1st, idx_2nd - idx_1st]
+        # score
+        idx_1st = element.content.strip.index('配点')
+        idx_2nd = element.content.strip.index('点', idx_1st+2)
+        score = element.content.strip[idx_1st+2, idx_2nd - (idx_1st+2)]
       end
-      # doc sanitize
-      doc = Sanitize.clean(element.inner_html, :elements => %w{
-          math
-          maction
-          maligngroup
-          malignmark
-          menclose
-          merror
-          mfenced
-          mfrac
-          mglyph
-          mi
-          mlabeledtr
-          mlongdiv
-          mmultiscripts
-          mn
-          mo
-          mover
-          mpadded
-          mphantom
-          mroot
-          mrow
-          ms
-          mscarries
-          mscarry
-          msgroup
-          msline
-          mspace
-          msqrt
-          msrow
-          mstack
-          mstyle
-          msub
-          msup
-          msubsup
-          mtable
-          mtd
-          mtext
-          mtr
-          munder
-          munderover
-          semantics
-          annotation
-          annotation-xml}
-      )
+      # qbody
+      element.css('div.qbody').each do |element|
+        # qnum
+        element.css('.qnum').each do |element|
+          qnum = element.content
+        end
+        # shomon1
+        element.css('.shomon1').each_with_index do |element, idx|
+          shomon1 = element.content
+        end
+        # doc sanitize
+        doc = Sanitize.clean(element.inner_html, :elements => %w{
+            math
+            maction
+            maligngroup
+            malignmark
+            menclose
+            merror
+            mfenced
+            mfrac
+            mglyph
+            mi
+            mlabeledtr
+            mlongdiv
+            mmultiscripts
+            mn
+            mo
+            mover
+            mpadded
+            mphantom
+            mroot
+            mrow
+            ms
+            mscarries
+            mscarry
+            msgroup
+            msline
+            mspace
+            msqrt
+            msrow
+            mstack
+            mstyle
+            msub
+            msup
+            msubsup
+            mtable
+            mtd
+            mtext
+            mtr
+            munder
+            munderover
+            semantics
+            annotation
+            annotation-xml}
+        )
+      end
+      f.write("#{refinfo},#{subject},#{qnum},#{shomon1},#{score},#{doc.gsub(/[\r\n]/,"").strip}\n")
     end
-    f.write("#{refinfo},#{subject},#{qnum},#{shomon1},#{score},#{doc.gsub(/[\r\n]/,"").strip}\n")
   end
 end
